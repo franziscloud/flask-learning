2d7927b5149da5ab696ea3f528ff5c946459062b
franziscloud <franziscloud@github.com>
2026-04-23 00:54:40 +0800

    added contact and projects function

--- app.py
+++ app.py
@@ -10,5 +10,22 @@ def home():
 def about():
     return '<h1>This is the about page.</h1>'
 
+@app.route('/contact')
+def contact():
+    return '''<h1>Franz Niño Gregorio</h1>
+              <hr>
+              <p>franz@flasklearning.com</p>'''
+              
+@app.route('/projects')
+def projects():
+    return '''<h3>Here are my projects:</h3>
+            <ul>
+                <li>Online Resume</li>
+                <li>Todo App</li>
+                <li>Barter Trade Site</li>
+            </ul>'''
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
